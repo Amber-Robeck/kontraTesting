f912f8b1b3ab224f1575a315292b1a93d9401833
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-08-08 19:01:00 -0500

    Added lib folder with minified kontra, removed pkg json and dependencies

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,50 @@
-import kontra from "kontra";
+let { init, Sprite, GameLoop } = kontra
 
-kontra.init();
+let { canvas } = init();
+
+let sprite = Sprite({
+    x: 100,        // starting x,y position of the sprite
+    y: 80,
+    color: 'red',  // fill color of the sprite rectangle
+    width: 20,     // width and height of the sprite rectangle
+    height: 40,
+    dx: 2          // move the sprite 2px to the right every frame
+});
+
+let loop = GameLoop({  // create the main game loop
+    update: function () { // update the game state
+        sprite.update();
+
+        // wrap the sprites position when it reaches
+        // the edge of the screen
+        if (sprite.x > canvas.width) {
+            sprite.x = -sprite.width;
+        }
+    },
+    render: function () { // render the game state
+        sprite.render();
+    }
+});
+
+loop.start();    // start the game
+
+
+// // import kontra from "kontra";
+// let { init, Sprite } = kontra;
+// let { canvas, context } = init();
+
+// let obstacles = Sprite({
+//     type: "obstacle",
+//     x: 50,
+//     y: 50,
+//     dx: Math.random() * 4 - 2,
+//     dy: Math.random() * 4 - 2,
+//     radius: 30,
+//     render() {
+//         this.context.strokeStyle = "white";
+//         this.context.beginPath();
+//         this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+//         this.context.stroke();
+//     },
+// });
+// obstacles.render();
